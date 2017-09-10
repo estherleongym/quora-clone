792f3527167824883fdea0547449df458759c958
Esther Leong Yuet Mei <estherleongyuetmei@Esthers-MacBook-Air.local>
2017-09-10 18:58:29 +0800

    Add Questions Button

---
+++ app/controllers/questions.rb
@@ -0,0 +1,24 @@
+get '/questions/ask' do
+
+  erb :"static/question"
+	
+end
+
+
+post '/questions/submit_question' do
+
+  @question = Question.new(params[:questions])
+
+  	if @question.save
+
+  		puts "Question submitted successfully!"
+
+  	else
+
+  		puts "Oops something went wrong!"
+
+  	end
+
+ erb :"static/session"
+
+end
