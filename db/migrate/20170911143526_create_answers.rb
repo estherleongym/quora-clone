c282b65f6e75853eafbc856e4abd27887018791e
Esther Leong Yuet Mei <estherleongyuetmei@Esthers-MacBook-Air.local>
2017-09-11 17:00:48 +0800

    Completed Answer Class and Migration

--- db/migrate/20170911143526_create_answers.rb
+++ db/migrate/20170911143526_create_answers.rb
@@ -1,11 +1,11 @@
-class CreateAnswers < ActiveRecord::Migration
+class CreateAnswers < ActiveRecord::Migration[5.0]
 
 	def change
 
 		create_table :answers do |t|
 
-		t.references :users, index: true
-		t.references :questions, index: true
+		t.references :users, index: true, foreign_key: true
+		t.references :questions, index: true, foreign_key: true
 		t.text :answer_text
 
 		t.timestamps null: false
